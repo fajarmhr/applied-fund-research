fd00fb9f8028889329601267672728df114569b2
masddika <mf.mahardika@gmail.com>
2023-03-07 03:19:22 +0700

    day 1

--- Cellular Network/ca4G/enb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ENB_LOC=`locate /src/srsenb | grep /srsenb/src/srsenb`
-
-sudo ${ENB_LOC} ./enb.conf $@
--- Cellular Network/pdt-repo/enb.sh
+++ Cellular Network/pdt-repo/enb.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-UHD_IMAGES_DIR="/usr/share/uhd/images"
-ENB_SERIAL="serial=F6113F"
-UHD_ARGS="--rf.device_args=\"${ENB_SERIAL}\""
+#ENB_LOC=`locate /src/srsenb | grep /srsenb/src/srsenb`
 
-sudo UHD_IMAGES_DIR=${UHD_IMAGES_DIR} srsenb ./enb.conf ${UHD_ARGS} $@
+sudo srsenb ./enb.conf $@
